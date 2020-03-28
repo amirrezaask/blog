571d20219b2bde34ee851006e51b53f03279e63a
amirrezaask <raskarpour@gmail.com>
2020-03-29 04:11:15 +0430

    posts function for wrapping posts parsing

--- src/blog/parser.clj
+++ src/blog/parser.clj
@@ -1,17 +1,20 @@
 (ns blog.parser
   (:require [clj-org.org :refer [parse-org]] [hiccup.core :as h]))
 
-(defn open-file
-  "Basically opens a file with given file-path"
-  [file-path]
-  (slurp file-path))
-
 (defn parse
   "first opens given file and parse it into hiccup tree."
   [file-path]
   (->
    file-path
-   (open-file)
+   (slurp)
    (parse-org)
    (:content)
    (h/html)))
+
+(defn posts
+  "parses all posts in /posts and return all posts html representation plus the posts index."
+  [posts-loc posts]
+  (->>
+   posts
+   (map (fn [post-name] (format "%s/%s" posts-loc post-name)))
+   (map parse)))

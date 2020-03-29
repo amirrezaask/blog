8054ec5bf90c68056e0f29862911dc3295b87682
amirrezaask <raskarpour@gmail.com>
2020-03-29 04:43:24 +0430

    WIP: index problem

--- src/blog/parser.clj
+++ src/blog/parser.clj
@@ -17,7 +17,11 @@
 (defn with-posts-index
   "generate posts index"
   [posts]
-  )
+  (let [index []]
+    (map (fn [{:keys [name content]}]
+         (conj index name) {:name name :content content}) posts)
+    (conj posts index)
+  ))
 
 (defn posts
   "parses all posts in /posts and return all posts html representation plus the posts index."
@@ -26,6 +30,6 @@
    posts
    (map (fn [post-name] (format "%s/%s" posts-loc post-name)))
    (map parse)
-   (posts-index)))
+   (with-posts-index)))
 
 (posts "posts" ["post1.org" "post2.org"])

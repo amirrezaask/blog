03b6d7a5c862a822e81fb1339b99322daa38ed79
amirrezaask <raskarpour@gmail.com>
2020-03-29 20:57:43 +0430

    fix index of posts

--- src/blog/parser.clj
+++ src/blog/parser.clj
@@ -1,8 +1,10 @@
 (ns blog.parser
   (:require [clj-org.org :refer [parse-org]]
-            [hiccup.core :as h]))
+            [hiccup.core :as h]
+            [clojure.string :as s]))
 
-(defn parse
+
+(defn read-and-parse
   "first opens given file and parse it into hiccup tree."
   [file-path]
   {
@@ -12,16 +14,13 @@
                (slurp)
                (parse-org)
                (:content)
-               (h/html))})
+               )})
 
 (defn with-posts-index
   "generate posts index"
-  [posts]
-  (let [index []]
-    (map (fn [{:keys [name content]}]
-         (conj index name) {:name name :content content}) posts)
-    (conj posts index)
-  ))
+  [posts-loc posts]
+  (let [names (map (fn [post] (:name post)) posts)]
+    (conj posts {:name (format "%s/index.html" posts-loc) :content [:ul (map (fn [name] [:li name]) names)]} )))
 
 (defn posts
   "parses all posts in /posts and return all posts html representation plus the posts index."
@@ -29,7 +28,11 @@
   (->>
    posts
    (map (fn [post-name] (format "%s/%s" posts-loc post-name)))
-   (map parse)
-   (with-posts-index)))
+   (map (fn [post] (read-and-parse post)))
+   (with-posts-index posts-loc)
+   (map (fn [post] {:name (:name post) :content (h/html (:content post))}))
+   (map (fn [post] {:name (format "%s.html" (get (s/split (:name post) #"\.") 0)) :content (:content post)}))
+   (map (fn [post] (spit (:name post) (:content post))))
+))
 
 (posts "posts" ["post1.org" "post2.org"])
